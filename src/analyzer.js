b6432a456f1d2d0eb948d5eae301fbe575dd98a5
CamiFlo <camila.flores.j@gmail.com>
2023-06-14 16:29:46 -0400

    HTML, funciones, CSS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,63 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
+
+  //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    return text.trim().length;
+    
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //return text.split(" ");  esta fue mi primera opción
+  getWordCount: (text) =>{
+    let words= text.split(" ");
+    return words.length;
+  
   },
+  
+  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
+      const regex = /\d+/g;
+      const matches = text.match(regex); 
+      if (matches) {
+        return matches.length;
+      } else {
+        return 0;
+      }
+    },
+    
+    //return text.replace[]
+    // isNaN o .replace ? expresión regular para buscarr numeros? No sirve para numeros negativos?
+  
   getNumberSum: (text) => {
+    let sum = 0;
+    let digitsAddUp = (text).split(""); // entrega un array
+    for(i=0; i< digitsAddUp.length; i++){ //recorre el arreglo buscando un numero y lo cuenta
+    const char = digitsAddUp[i];
+    if (!isNan(char)){
+      sum += parseInt(char);
+       }
+    }
+  return sum;
+},
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  
+  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  //Sacar promedio dividiendo el total de caracteres sin espacios/total de palabras
+  getAverageWordLength: (text) => {    
+    const words = text.split(" ");
+    let totalLength = words.reduce(function (acc, word){    //length TH TH TH!!!
+    return acc + word.length;
+    }, 0)
+    const averageLength = totalLength / words.length;
+    return number(averageLength).toFixed(2);
+  }
+
+    
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 import analyzer from './analyzer.js';
 
+//const userText = document.querySelector('li[name="textContent"]')
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-document.getElementById('button').reset();
+
+//actualizar parametros del texto
+const textarea= document.querySelector('textarea[name="user-input"]');
+
+textarea.addEventListener("keyup", function(){
+const result= textarea.value;
+
+//contar caracteres
+//list list1[0].innerHTML="caracteres"+ analyzer.getCharacterCount(textarea,value);
+document.querySelector('.list1').innerHTML= "caracteres"+ analyzer.getCharacterCount(result);
+
+//caracteres sin espacio
+list list2[1].innerHTML="caracteres sin espacio"+ analyzer.getCharacterCountExcludingSpaces(textarea,value);
+//contador palabras
+list list3[2].innerHTML="palabras"+ analyzer.getWordCount(textarea,value);
+//contador numeros
+list list4[3].innerHTML="números"+ analyzer.getNumberCount(textarea,value);
+//contador suma numeros
+list list5[4].innerHTML="suma números"+ analyzer.getNumberSum(textarea,value);
+//longitud promedio palabras
+list list6[5].innerHTML="promedio longitud"+ analyzer.getAverageWordLength(textarea,value);
+),}
+
+//boton reset
+const btnReset= document.getElementById("reset-button");
+btnReset.addEventListener("click",function(){
+  textBox.value="";
+});
+
+//deberian borrarse todos los campos de parametros?
