3870089b703c7772bd794a67cf6e7aa50fc5ebb1
CamiFlo <camila.flores.j@gmail.com>
2023-06-15 13:09:47 -0400

    cambios miercoles 14

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,4 +1,4 @@
-const analyzer = {  
+const analyzer = {
   //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   getCharacterCount: (text) => {
     return text.length;
@@ -6,58 +6,65 @@ const analyzer = {
 
   //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   getCharacterCountExcludingSpaces: (text) => {
-    return text.trim().length;
-    
+    /*const text= (text);
+    const specialChar = [".",",",";",":","«","»","[","[","]","{","}","(",")","¿","?","¡","!","-"];
+    const ExcludingSpecialChar = text - specialChar;
+    return ExcludingSpecialChar.trim().length;*/
+    const regex = /[^\s.,;:«»[\]{}()¿?¡!-]/g;
+    const matches = text.match(regex);
+    return matches = matches.length;
+
   },
 
   //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    //return text.split(" ");  esta fue mi primera opción
-  getWordCount: (text) =>{
-    let words= text.split(" ");
+  //return text.split(" ");  esta fue mi primera opción
+  getWordCount: (text) => {
+    const words = text.split(" ");
     return words.length;
-  
+
   },
-  
+
   //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  
+
   getNumberCount: (text) => {
-      const regex = /\d+/g;
-      const matches = text.match(regex); 
-      if (matches) {
-        return matches.length;
-      } else {
-        return 0;
-      }
-    },
-    
-    //return text.replace[]
-    // isNaN o .replace ? expresión regular para buscarr numeros? No sirve para numeros negativos?
-  
+    const regex = /\d+/g;
+    const matches = text.match(regex);
+    if (matches) {
+      return matches.length;
+    } else {
+      return 0;
+    }
+  },
+
+  //return text.replace[]
+  // isNaN o .replace ? expresión regular para buscarr numeros? No sirve para numeros negativos?
+
   getNumberSum: (text) => {
     let sum = 0;
-    let digitsAddUp = (text).split(""); // entrega un array
-    for(i=0; i< digitsAddUp.length; i++){ //recorre el arreglo buscando un numero y lo cuenta
-    const char = digitsAddUp[i];
-    if (!isNan(char)){
-      sum += parseInt(char);
-       }
+    const digitsAddUp = (text).split(""); // entrega un array
+    for(let i=0; i< digitsAddUp.length; i++) { //recorre el arreglo buscando un numero y lo cuenta
+      const char = digitsAddUp[i];
+      if(!isNaN(char[i])) {
+        sum += parseInt(char[i]);
+      }
     }
-  return sum;
-},
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  
+    return sum;
+  },
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+
   //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   //Sacar promedio dividiendo el total de caracteres sin espacios/total de palabras
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     const words = text.split(" ");
-    let totalLength = words.reduce(function (acc, word){    //length TH TH TH!!!
-    return acc + word.length;
+    const totalLength = words.reduce(function (acc, word) {    //length TH TH TH!!!
+      return acc + word.length;
     }, 0)
     const averageLength = totalLength / words.length;
-    return number(averageLength).toFixed(2);
+    return parseFloat(averageLength).toFixed(2);
+    //number(averageLength).toFixed(2);
   }
 
-    
+
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -5,31 +5,34 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 //actualizar parametros del texto
-const textarea= document.querySelector('textarea[name="user-input"]');
+const textarea = document.querySelector('textarea[name="user-input"]');
+const lista = document.querySelectorAll('li')
+textarea.addEventListener('keyup', function () {
+  const result = textarea.value;
 
-textarea.addEventListener("keyup", function(){
-const result= textarea.value;
+  //contar caracteres
+  //list list1[0].innerHTML="caracteres"+ analyzer.getCharacterCount(textarea,value);
+  //document.querySelector('.list1').innerHTML = "caracteres" + analyzer.getCharacterCount(result);
+  //const wordCountElement = document.querySelector('.parrafo');
 
-//contar caracteres
-//list list1[0].innerHTML="caracteres"+ analyzer.getCharacterCount(textarea,value);
-document.querySelector('.list1').innerHTML= "caracteres"+ analyzer.getCharacterCount(result);
-
-//caracteres sin espacio
-list2[1].innerHTML="caracteres sin espacio"+ analyzer.getCharacterCountExcludingSpaces(result);
-//contador palabras
-list3[2].innerHTML="palabras"+ analyzer.getWordCount(result);
-//contador numeros
-list4[3].innerHTML="números"+ analyzer.getNumberCount(result);
-//contador suma numeros
-list5[4].innerHTML="suma números"+ analyzer.getNumberSum(result);
-//longitud promedio palabras
-list6[5].innerHTML="promedio longitud"+ analyzer.getAverageWordLength(result);
-),}
+  lista[0].innerHTML="caracteres "+ analyzer.getCharacterCount(textarea.value);
+  //caracteres sin espacio
+  lista[1].innerHTML = "caracteres sin espacio " + analyzer.getCharacterCountExcludingSpaces(result);
+  //contador palabras
+  lista[2].innerHTML = "palabras " + analyzer.getWordCount(result);
+  //contador numeros
+  lista[3].innerHTML = "números " + analyzer.getNumberCount(result);
+  //contador suma numeros
+  lista[4].innerHTML = "suma números " + analyzer.getNumberSum(result);
+  //longitud promedio palabras
+  lista[5].innerHTML = "promedio longitud " + analyzer.getAverageWordLength(result);
+});
 
 //boton reset
-const btnReset= document.getElementById("reset-button");
-btnReset.addEventListener("click",function(){
-  textBox.value="";
+const btnReset = document.getElementById('reset-button');
+btnReset.addEventListener('click', () => {
+    
+  textarea.value = '';
 });
 
 
